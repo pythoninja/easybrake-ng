18b080b86c2f128162ac51c7d236394f792d0281
pythoninja <pythoninja@users.noreply.github.com>
2024-03-30 20:14:18 +0300

    fix(app): add run note after finish

--- src/run.py
+++ src/run.py
@@ -29,6 +29,7 @@ def easybrake_runner(input_dir: Path, output_dir: Path, preset_path: Path) -> No
     command_generator = CommandGenerator(preset=preset, movies=movies, output_dir=target_dir)
     commands: list[str] = command_generator.get()
 
+    print("# Generated hanbrake commands, just paste it to the bash (or similar) file")
     print(*commands, sep="\n\n")
 
 
