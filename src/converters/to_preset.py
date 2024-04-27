77eaae59078f7d0aa3447c5577c954dd7962119a
pythoninja <pythoninja@users.noreply.github.com>
2024-04-27 19:12:56 +0300

    feat: load preset from url

--- src/converters/to_preset.py
+++ src/converters/to_preset.py
@@ -1,53 +1,92 @@
 import json
 import sys
+import tempfile
 from pathlib import Path
-from typing import Any
 
 from loguru import logger
 
 from src.dtos.preset import Preset
+from src.utils.httpclient import HttpClient
+from src.utils.url import URLUtils
+from src.enums.location import LocationType
+from src.named_type import PresetJSON, PresetProperyValues
 
 
-class FilePresetConverter:
-    def __init__(self, preset_path: Path):
-        self.preset_path = preset_path
-        self.preset_json: dict[str, Any] | None = self.parse_json()
+class PresetConverter:
+    def __init__(self, preset_location: str):
+        self.full_preset_path = None
+        self.preset_location = preset_location
+        self.preset_json: PresetJSON | None = self.__load_preset()
 
-    def parse_json(self) -> dict[str, Any]:
+    def __load_preset(self) -> PresetJSON:
+        location_type = URLUtils.get_location_type(self.preset_location)
+
+        match location_type:
+            case LocationType.DISK:
+                return self.__load_from_disk()
+            case LocationType.URL:
+                return self.__load_from_url()
+
+    def __load_from_disk(self) -> PresetJSON:
+        logger.info("Preset is on a disk: {}", self.preset_location)
+        logger.info("Reading it...")
+        self.full_preset_path = Path(self.preset_location)
+
+        return self.__read_json()
+
+    def __load_from_url(self) -> PresetJSON:
+        logger.info("Preset is an url: {}", self.preset_location)
+        logger.info("Downloading it...")
+        client = HttpClient()
+        response = client.get(self.preset_location)
+
+        if client.is_error():
+            logger.error("Error downloading preset: {}", response.status_code)
+            sys.exit(1)
+
+        temp_file = self.save_temp_preset(response.text)
+        self.full_preset_path = temp_file
+
+        return self.__read_json()
+
+    def __read_json(self) -> PresetJSON:
         try:
-            with self.preset_path.open(encoding="UTF-8") as preset_file:
-                return json.load(preset_file)
-        except FileNotFoundError:
-            logger.error("File {} does not exist.", self.preset_path)
-            sys.exit(1)
-        except json.JSONDecodeError:
-            logger.error("File {} is not a valid JSON.", self.preset_path)
-            sys.exit(1)
+            with self.full_preset_path.open(encoding="UTF-8") as f:
+                return json.load(f)
+        except (FileNotFoundError, json.JSONDecodeError) as e:
+            self.__handle_json_exceptions(e)
 
-    def get(self) -> Preset:
-        return Preset(
-            name=self.__get_name(),
-            picture_width=self.__get_picture_width(),
-            picture_height=self.__get_picture_height(),
-            path=self.preset_path,
-            upscale_enabled=self.__get_upscale(),
-        )
+    def __handle_json_exceptions(self, exception: FileNotFoundError | json.JSONDecodeError) -> None:
+        if isinstance(exception, FileNotFoundError):
+            logger.error(f"File {self.preset_location} does not exist.")
 
-    def __get_name(self) -> str:
-        return self.__get_preset_property("PresetName")
+        if isinstance(exception, json.JSONDecodeError):
+            logger.error(f"File {self.preset_location} is not a valid JSON.")
 
-    def __get_picture_width(self) -> int:
-        return self.__get_preset_property("PictureWidth")
+        sys.exit(1)
 
-    def __get_picture_height(self) -> int:
-        return self.__get_preset_property("PictureHeight")
-
-    def __get_upscale(self) -> bool:
-        return self.__get_preset_property("PictureAllowUpscaling")
-
-    def __get_preset_property(self, property_name: str) -> str | int | bool:
+    def __get_preset_property(self, property_name: str) -> PresetProperyValues:
         try:
             return self.preset_json["PresetList"][0][property_name]
         except (KeyError, TypeError):
             logger.error("Invalid JSON structure: Missing {} key.", property_name)
             sys.exit(1)
+
+    def get(self) -> Preset:
+        return Preset(
+            name=self.__get_preset_property("PresetName"),
+            picture_width=self.__get_preset_property("PictureWidth"),
+            picture_height=self.__get_preset_property("PictureHeight"),
+            path=self.full_preset_path,
+            upscale_enabled=self.__get_preset_property("PictureAllowUpscaling"),
+        )
+
+    def save_temp_preset(self, content: str) -> Path:
+        temp_dir = tempfile.mkdtemp(prefix="preset_")
+        temp_file = Path(temp_dir).joinpath("preset.json")
+        logger.info("Saving preset to a temp file: {}", temp_file)
+
+        with temp_file.open("w", encoding="UTF-8") as f:
+            f.write(content)
+
+        return temp_file
---
+++ src/utils/url.py
@@ -0,0 +1,23 @@
+from typing import AnyStr
+from urllib.parse import urlparse
+
+from loguru import logger
+
+from src.enums.location import LocationType
+
+
+class URLUtils:
+    @staticmethod
+    def get_location_type(url: str) -> LocationType:
+        scheme = URLUtils.extract_scheme(url)
+        logger.info("Scheme type: {}", scheme or "disk")
+
+        match scheme:
+            case "https" | "http":
+                return LocationType.URL
+            case _:
+                return LocationType.DISK
+
+    @staticmethod
+    def extract_scheme(url: str) -> AnyStr:
+        return urlparse(url).scheme
